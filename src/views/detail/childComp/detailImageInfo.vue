2ad3a5b4f4783eb023212ea6adcdfd17ff6c97f2
cangxueyi <3229236440@qq.com>
2021-08-09 21:19:19 +0800

    详情页信息

---
+++ src/views/detail/childComp/detailImageInfo.vue
@@ -0,0 +1,39 @@
+<template>
+	<!-- 判断接口内的数据是否为空 -->
+	<div v-if="" class="goods-info">
+		<div class="">
+			<div class="start"></div>
+			<!-- 其他信息 -->
+			<div class="desc">{{}}</div>
+			<div class="end"></div>
+		</div>
+		<!-- 遍历数据中的图片 -->
+		<div class="">
+			<!-- 绑定key -->
+			<div class=""></div>
+			<!-- 遍历图片 -->
+			<div class="">
+				<img src="" />
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default{
+		name:"DetailImageInfo",
+		props:{
+			//接口
+			detailInfo:{
+				type:Object,
+				default(){
+					return{}
+				}
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	
+</style>
---
+++ src/views/detail/childComp/detailParamsInfo.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="">
+		<!-- 展示参数信息 -->
+		<!-- 遍历table时外层不加div -->
+		<table>
+			<tr>
+				<td></td>
+			</tr>
+		</table>
+	</div>
+</template>
+
+<script>
+	export default{
+		name:"DetailParamsInfo",
+		props:{
+			paramsInfo:{
+				type:Object,
+				default(){
+					return{}
+				}
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
